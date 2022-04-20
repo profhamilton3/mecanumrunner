5b631be17a8be05f14401db549ab3eae6544ae64
T. Hamilton <profhamilton3@gmail.com>
2022-04-19 21:53:06 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,21 +2,21 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (0 == receivedNumber) {
         wuKong.mecanumStop()
     } else if (1 == receivedNumber) {
-        wuKong.mecanumRun(wuKong.RunList.Front, 75)
+        wuKong.mecanumRun(wuKong.RunList.Front, 100)
     } else if (2 == receivedNumber) {
-        wuKong.mecanumRun(wuKong.RunList.left, 50)
+        wuKong.mecanumRun(wuKong.RunList.left, 100)
     } else if (3 == receivedNumber) {
-        wuKong.mecanumRun(wuKong.RunList.right, 50)
+        wuKong.mecanumRun(wuKong.RunList.right, 100)
     } else if (4 == receivedNumber) {
-        wuKong.mecanumRun(wuKong.RunList.rear, 50)
+        wuKong.mecanumRun(wuKong.RunList.rear, 100)
     } else if (5 == receivedNumber) {
-        wuKong.mecanumRun(wuKong.RunList.LeftFront, 50)
+        wuKong.mecanumRun(wuKong.RunList.LeftFront, 100)
     } else if (6 == receivedNumber) {
-        wuKong.mecanumRun(wuKong.RunList.RightFront, 50)
+        wuKong.mecanumRun(wuKong.RunList.RightFront, 100)
     } else if (7 == receivedNumber) {
-        wuKong.mecanumRun(wuKong.RunList.LeftRear, 50)
+        wuKong.mecanumRun(wuKong.RunList.LeftRear, 100)
     } else if (8 == receivedNumber) {
-        wuKong.mecanumRun(wuKong.RunList.RightRear, 50)
+        wuKong.mecanumRun(wuKong.RunList.RightRear, 100)
     } else if (9 == receivedNumber) {
         wuKong.mecanumRun(wuKong.RunList.stop, 0)
     } else if (10 == receivedNumber) {
@@ -24,9 +24,9 @@ radio.onReceivedNumber(function (receivedNumber) {
     } else if (11 == receivedNumber) {
         wuKong.mecanumDrift(wuKong.TurnList.Right)
     } else if (12 == receivedNumber) {
-        wuKong.mecanumSpin(wuKong.TurnList.Left, 40)
+        wuKong.mecanumSpin(wuKong.TurnList.Left, 100)
     } else if (13 == receivedNumber) {
-        wuKong.mecanumSpin(wuKong.TurnList.Right, 40)
+        wuKong.mecanumSpin(wuKong.TurnList.Right, 100)
     } else {
         basic.showIcon(IconNames.No)
     }
@@ -40,3 +40,4 @@ wuKong.ServoList.S3,
 wuKong.ServoList.S4
 )
 basic.showIcon(IconNames.Tortoise)
+wuKong.setLightMode(wuKong.LightMode.BREATH)
