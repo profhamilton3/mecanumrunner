c1f4c8a8410360cd223a43483c7eb75bf2be18a1
T. Hamilton <profhamilton3@gmail.com>
2022-04-23 17:04:37 -0400

    update speeds. -sync radio set group with remote

--- main.ts
+++ main.ts
@@ -4,19 +4,19 @@ radio.onReceivedNumber(function (receivedNumber) {
     } else if (1 == receivedNumber) {
         wuKong.mecanumRun(wuKong.RunList.Front, 100)
     } else if (2 == receivedNumber) {
-        wuKong.mecanumRun(wuKong.RunList.left, 50)
+        wuKong.mecanumRun(wuKong.RunList.left, 100)
     } else if (3 == receivedNumber) {
-        wuKong.mecanumRun(wuKong.RunList.right, 50)
+        wuKong.mecanumRun(wuKong.RunList.right, 100)
     } else if (4 == receivedNumber) {
         wuKong.mecanumRun(wuKong.RunList.rear, 100)
     } else if (5 == receivedNumber) {
-        wuKong.mecanumRun(wuKong.RunList.LeftFront, 50)
+        wuKong.mecanumRun(wuKong.RunList.LeftFront, 100)
     } else if (6 == receivedNumber) {
-        wuKong.mecanumRun(wuKong.RunList.RightFront, 50)
+        wuKong.mecanumRun(wuKong.RunList.RightFront, 100)
     } else if (7 == receivedNumber) {
-        wuKong.mecanumRun(wuKong.RunList.LeftRear, 50)
+        wuKong.mecanumRun(wuKong.RunList.LeftRear, 100)
     } else if (8 == receivedNumber) {
-        wuKong.mecanumRun(wuKong.RunList.RightRear, 50)
+        wuKong.mecanumRun(wuKong.RunList.RightRear, 100)
     } else if (9 == receivedNumber) {
         wuKong.mecanumRun(wuKong.RunList.stop, 0)
     } else if (10 == receivedNumber) {
@@ -24,9 +24,9 @@ radio.onReceivedNumber(function (receivedNumber) {
     } else if (11 == receivedNumber) {
         wuKong.mecanumDrift(wuKong.TurnList.Right)
     } else if (12 == receivedNumber) {
-        wuKong.mecanumSpin(wuKong.TurnList.Left, 50)
+        wuKong.mecanumSpin(wuKong.TurnList.Left, 75)
     } else if (13 == receivedNumber) {
-        wuKong.mecanumSpin(wuKong.TurnList.Right, 50)
+        wuKong.mecanumSpin(wuKong.TurnList.Right, 75)
     } else {
         basic.showIcon(IconNames.No)
     }
@@ -39,5 +39,5 @@ wuKong.ServoList.S2,
 wuKong.ServoList.S3,
 wuKong.ServoList.S4
 )
-basic.showIcon(IconNames.Scissors)
-wuKong.setLightMode(wuKong.LightMode.BREATH)
+basic.showIcon(IconNames.Asleep)
+wuKong.setLightMode(wuKong.LightMode.OFF)
